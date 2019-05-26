d0c180c911df5426d1272f4da0f1f0820d070081
Lynch Mwaniki <30522249+LynchSKM@users.noreply.github.com>
2019-05-26 23:20:38 +0200

    Fixed matrix multiplication to be generic
    Modified the indexing used in the matrix multiplication to allow for it to be generic. You can now safely do matrix multiplication in parallel. Race conditions are avoided by ensuring that threads are given separate elements in Matrix C to calculate using the chunk_size in the Loop schedule.

--- tutQ3.c
+++ tutQ3.c
@@ -1,15 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
+#include <math.h>
 
 #define num_threads 8
 
-int getElementLocation2D(int index[2], int N)
+int getElementLocation2D(int index[2], int number_of_columns)
 {
 	int row    = index[0];
 	int column = index[1];
 
-	return ((row*N)+column);
+	return ((row*number_of_columns)+column);
 }
 
 int* allocateMarix(int number_of_elements)
@@ -18,40 +19,88 @@ int* allocateMarix(int number_of_elements)
 	return result;
 }
 
-int retrieveElement2D(int* matrix, int index[2], int N)
+int retrieveElement2D(int* matrix, int index[2], int number_of_columns)
 {
-	return *(matrix+getElementLocation2D(index, N));
+	return *(matrix+getElementLocation2D(index, number_of_columns));
 }
 
-int* rank2TensorMult(int *matrixA, int *matrixB, int N)
+int* rank2TensorMult(int *matrixA, int dimsA[2], int *matrixB, int dimsB[2], int dimsC[2])
 {
-	int number_of_elements = 4*N; // let N represent k
+	if(dimsA[1] != dimsB[0]) 
+	{
+		printf("Matrices are not the same size!");
+		return (int*)NULL;
+	}
+	
+	int rows_in_A = dimsA[0];
+	int cols_in_A = dimsA[1];
+	int number_of_elements = dimsC[0]*dimsC[1]; // let N represent k
 	int* result = allocateMarix(number_of_elements);
 	if(result!=NULL)
 	{
-		int total_multiplication_operations = N*number_of_elements;
+		int total_multiplication_operations = cols_in_A*number_of_elements;
 		
-		#pragma omp parallel for 
-		for(int m=0; m<total_multiplication_operations; m++)
-		{
-			int i = m/(2*N);
-			int j = m%(2*N);
-			int k = m/(2*N*N);
+		omp_set_num_threads(num_threads);
+		#pragma omp parallel
+		{	
+			int chunk_size = round(number_of_elements/(float)omp_get_num_threads())*cols_in_A;
+			// printf("Calculated chunk_size = %d \n", chunk_size);
+			int rowA = 0;
+			int colA = 0;
 			
-			//printf("i = %d, j = %d, k = %d\n", i, j, k);
+			int rowB = 0;
+			int colB = 0;
 			
-			int indexA[2] = {i, k}; 
-			int indexB[2] = {i, j};
+			int rowC = 0;
+			int colC = 0;
+			/* statically allocate chunks so that each thread works on a certain range of numbers
+			   in matrixC. This will avoid any race conditions as we ensure that a thread does all the 
+			   multiplication and addition to compute an element in matrix C.
+		       Basically, each thread does the entire computation to get an element. They repeat this for the 
+			   chunk given to them.
+			*/
+			#pragma omp for schedule(static, chunk_size) 
+			for(int mult_op=0; mult_op<total_multiplication_operations; mult_op++)
+			{
+				// Get matrix A indexes:
+				int tmp = mult_op/(total_multiplication_operations/rows_in_A);
+				if(tmp % rows_in_A) rowA = tmp;
+				colA = mult_op % cols_in_A;
+				
+				// Get matrix B indexes:
+				rowB = colA;
+				colB = ((int)(mult_op/dimsB[0]))%dimsB[1]; // multiplication_index divided by row in matrix B 
+				//and mod by column length of matrix B.
+	
+				
+				// Get matrix C indexes:
+				rowC = rowA;
+				colC = colB;
+				/*
+				printf("Thread = %d \n", (int)omp_get_thread_num());
+				printf("Multiplication index %d\n", mult_op);
+				printf("A indexes i = %d, j = %d\n", rowA, colA);
+				printf("B indexes i = %d, j = %d\n", rowB, colB);
+				printf("C indexes i = %d, j = %d\n", rowC, colC);
+				*/
+				
+				
+				int indexA[2] = {rowA, colA}; 
+				int indexB[2] = {rowB, colB};
+				
+				int elementA = retrieveElement2D(matrixA, indexA, cols_in_A);
+				int elementB = retrieveElement2D(matrixB, indexB, dimsB[1]);
 			
-			int elementA = matrixA[i];
-			int elementB = matrixB[((i%2)*(2*N))+j];
-			
-			//printf("A = %d, B = %d\n", elementA, elementB);
-			
-			int indexC[2] = {k, j};
-			int* total = result+getElementLocation2D(indexC, 2*N);
-			*total+=(elementA*elementB);
+				//printf("A = %d, B = %d\n", elementA, elementB);
+				
+				int indexC[2] = {rowC, colC};
+				int* total = result+getElementLocation2D(indexC, dimsC[1]);
+				*total+=(elementA*elementB);
+
+				//printf("========================\n");
+			}
 		}
+		//printf("Done!\n");
 	}
 	else	
 	{
@@ -80,18 +129,29 @@ void print2DMatrix(int *matrix, int N, int M)
 
 int main()
 {
-	int A[32] = {1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8};
-	int B[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+	int B[32] = {10, 25,  3,  4, 16, 27, 18,  1, 
+		     10, 33, 45,  5, 46, 17, 38, 71, 
+		     22,  3, 40, 51, 63, 75,  8,  1, 
+		      2, 30, 49, 53, 66, 72, 83,  5};
 	
-	int k = 4;
-	print2DMatrix(A,2, k);
-	print2DMatrix(B,k, 2*k);
+	int A[8] = {1, 2, 3, 4, 12, 25, 37, 49};
 	
-	int *results = rank2TensorMult(A, B,k);
-	print2DMatrix(results,2, 2*k);
-
-
+	int rowA = 2, colA = 4;
+	int rowB = colA, colB = 8;
+	int rowC = rowA, colC = colB;
+	
+	int dimsA[2] = {rowA,colA}, dimsB[2] = {rowB,colB}, dimsC[2] = {rowC, colC};
+	
+	printf("Matrix A \n");
+	print2DMatrix(A,rowA, colA);
+	printf("Matrix B \n");
+	print2DMatrix(B,rowB, colB);
+	
+	printf("\nMatrix C \n");
+	int *results = rank2TensorMult(A, dimsA, B, dimsB, dimsC);
+	print2DMatrix(results,rowC, colC);
+	
+	free(results);
 
 	return 0;
 }
-
